5e1c884342f3c05de7dd76d7746f094495668875
Danis Ozdemir <ozdemirda@gmail.com>
2024-07-18 10:34:45 +0100

    Fixing the deadlocks happening because of the "'ROLLBACK TO' does not 'RELEASE' savepoints" issue

--- driver_test.go
+++ driver_test.go
@@ -177,6 +177,9 @@ func TestRollback(t *testing.T) {
 	}
 
 	{
+		// The ctx on the left here is a new ctx and has a different
+		// savepoint stored in it. This is crucial, otherwise we'd lose
+		// the previous savepoint.
 		ctx, err := pool.Savepoint(ctx)
 		if err != nil {
 			t.Fatal(err)
@@ -191,14 +194,251 @@ func TestRollback(t *testing.T) {
 			t.Fatal(err)
 		}
 		if v != "hello world" {
-			t.Error("read back value from nested savepoint", v)
+			t.Fatal("failed to verify the existence of written data", v)
 		}
 	}
 
-	// top-level nesting can invalidate everything
+	// top-level nesting can invalidate everything, the table should be empty
 	pool.Rollback(ctx)
 	var w int
 	if err := pool.Exec(ctx, "select count(a) from t").ScanOne(&w); err != nil {
+		t.Fatal(err)
+	}
+	if w != 0 {
+		t.Fatal("read back value from top-level savepoint", w)
+	}
+}
+
+// The following function tries to run the sqlite statement 'VACUUM;'
+// through a brand new connection that it acquires from the connection
+// 'pool' (the first argument) in a new goroutine. The second argument
+// named as 'shouldFail' tells the function whether it should expect
+// a 'the database file is busy' error. It returns an error if the
+// real result of the attempt and the expected result do not match.
+func runVacuumViaNewConnectionInNewGoroutine(pool *Connections,
+	shouldFail bool) error {
+	errChan := make(chan error)
+	defer close(errChan)
+
+	go func() {
+		err := pool.Exec(context.Background(), "VACUUM;").Err()
+		if shouldFail {
+			if err == nil {
+				errChan <- fmt.Errorf("err should NOT have been nil")
+				return
+			} else if err.Error() != "the database file is busy" {
+				errChan <- fmt.Errorf("unexpected error: %s", err.Error())
+				return
+			}
+			errChan <- nil
+			return
+		}
+		errChan <- err
+	}()
+
+	err := <-errChan
+	return err
+}
+
+func TestRollbackTo(t *testing.T) {
+	pool, err := OpenPool(t.TempDir() + "/db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	if err := pool.Exec(ctx, "create table t (a)").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	txCtx, err := pool.Savepoint(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := pool.Exec(txCtx, "insert into t (a) values (?)", "hello world").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	var v string
+	if err := pool.Exec(txCtx, "select a from t").ScanOne(&v); err != nil {
+		t.Fatal(err)
+	}
+	if v != "hello world" {
+		t.Fatal("failed to verify existence of written data", v)
+	}
+
+	// RollbackTo, the changes are discarded, and the transaction is NOT
+	// cancelled. Therefore the following VACUUM attempt made from a different
+	// goroutine on a different connection should FAIL.
+	err = pool.RollbackTo(txCtx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The table should be empty
+	var w int
+	if err := pool.Exec(txCtx, "select count(a) from t").ScanOne(&w); err != nil {
+		t.Fatal(err)
+	}
+	if w != 0 {
+		t.Error("read back value from top-level savepoint", w)
+	}
+
+	err = runVacuumViaNewConnectionInNewGoroutine(pool, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The next VACUUM attempt should succeed, as the function Release
+	// is getting executed here.
+	err = pool.Release(txCtx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = runVacuumViaNewConnectionInNewGoroutine(pool, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRollbackWithoutRelease(t *testing.T) {
+	pool, err := OpenPool(t.TempDir() + "/db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	if err := pool.Exec(ctx, "create table t (a)").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	txCtx, err := pool.Savepoint(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := pool.Exec(txCtx, "insert into t (a) values (?)", "hello world").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	var v string
+	if err := pool.Exec(txCtx, "select a from t").ScanOne(&v); err != nil {
+		t.Fatal(err)
+	}
+	if v != "hello world" {
+		t.Fatal("failed to verify existence of written data", v)
+	}
+
+	// Rollback, the changes are not committed, and the transaction is
+	// cancelled. Therefore the following VACUUM attempt made from a different
+	// goroutine on a different connection should succeed without any issues.
+	err = pool.Rollback(txCtx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = runVacuumViaNewConnectionInNewGoroutine(pool, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The table should be empty
+	var w int
+	if err := pool.Exec(txCtx, "select count(a) from t").ScanOne(&w); err != nil {
+		t.Fatal(err)
+	}
+	if w != 0 {
+		t.Error("read back value from top-level savepoint", w)
+	}
+}
+
+func TestRollbackNestedSavepointsWithoutRelease(t *testing.T) {
+	pool, err := OpenPool(t.TempDir() + "/db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	if err := pool.Exec(ctx, "create table t (a)").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	txCtx1, err := pool.Savepoint(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := pool.Exec(txCtx1, "insert into t (a) values (?)", "hello world").Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	var v string
+	if err := pool.Exec(txCtx1, "select a from t").ScanOne(&v); err != nil {
+		t.Fatal(err)
+	}
+	if v != "hello world" {
+		t.Fatal("failed to verify existence of written data", v)
+	}
+
+	{
+		// The new savepoint is stored in a different context.
+		txCtx2, err := pool.Savepoint(txCtx1)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err = pool.Exec(txCtx2, "insert into t (a) values (?)", "hi there").Err(); err != nil {
+			t.Fatal(err)
+		}
+
+		// The table should contain two entries here
+		var w int
+		if err := pool.Exec(txCtx2, "select count(a) from t").ScanOne(&w); err != nil {
+			t.Fatal(err)
+		}
+		if w != 2 {
+			t.Error("read back value from top-level savepoint", w)
+		}
+
+		// The inner Rollback, the changes are not committed, and the transaction
+		// (the implicit one that was created when the first savepoint was
+		// introduced) is not affected. So the VACUUM attempt below should FAIL.
+		err = pool.Rollback(txCtx2)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// The table should now contain only one entry, because of the rollback
+		if err := pool.Exec(txCtx2, "select count(a) from t").ScanOne(&w); err != nil {
+			t.Fatal(err)
+		}
+		if w != 1 {
+			t.Error("read back value from top-level savepoint", w)
+		}
+
+		err = runVacuumViaNewConnectionInNewGoroutine(pool, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Outer Rollback, the changes are not committed, and implicit transaction
+	// is cancelled. So the following VACUUM attempt should succeed.
+	err = pool.Rollback(txCtx1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = runVacuumViaNewConnectionInNewGoroutine(pool, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// And the table should be empty
+	var w int
+	if err := pool.Exec(txCtx1, "select count(a) from t").ScanOne(&w); err != nil {
 		t.Fatal(err)
 	}
 	if w != 0 {
